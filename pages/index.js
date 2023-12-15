f59943ebbcf6d490b559b3ebac8cff033e79fe09
tamutamu2023 <tamuragenta@au.com>
2023-12-15 13:35:01 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,27 +18,7 @@ function EachPost() {
   )
 }
 
-export default function Posts() {
-  return (
-    <section>
-      <h2>おすすめの記事</h2>
-      <EachPost />
-      <EachPost />
-    </section>
-  )
-}
-
-function EachPost(props) {
-  return (
-    <article>
-      <a href={props.url}>
-        <h3>{props.title}</h3>
-      </a>
-    </article>
-  )
-}
-
-export default function Posts() {
+function Posts() {
   return (
     <section>
       <h2>おすすめの記事</h2>
